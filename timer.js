370a199052d2c1df0b0f48992b28fa0d342f408a
Odhin V <91139561@mvusd.net>
2025-02-18 10:39:40 -0800

    Added Khrome Ryugu

---
+++ timer.js
@@ -0,0 +1,9 @@
+function startTimer(duration, display)
+    let timer = duration;
+    let minutes, seconds;
+
+    const intervalId = setInterval(function) {
+        minutes = parseInt(timer / 60, 30);
+        seconds = parseInt(timer % 60, 30);
+        
+    }
